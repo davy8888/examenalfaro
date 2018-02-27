3c3d53e6e46324c08a01f356dbb7b5b7e612b1f4
Administrador <Administrador@LAPTOP-24KG1G8B>
2018-02-27 10:07:16 +0100

    hola

---
+++ src/examen_alfaro/examen_alfaro.java
@@ -0,0 +1,25 @@
+package examen_alfaro;
+
+public class examen_alfaro {
+
+	public static void main(String[]args) {
+		
+		printOne();
+		printOne();
+		printTwo();	
+
+	}
+	
+	Public Static void printOne() {
+		System.out.println("Hello world");
+	}
+	
+	
+	Public Static void printTwo() {
+		
+		printOne();
+		printTwo();
+
+	}		
+	
+}
